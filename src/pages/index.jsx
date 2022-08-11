539e9fc730db4b05ecfaeaef2ba44e73d0b07ba0
Mark Tellez <mark@devmentor.live>
2022-08-11 16:09:08 -0500

    start here

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,152 @@
 export default function Homepage() {
   return (
-    <div className="mt-16 container mx-auto">
-      <h1>Hello world.</h1>
+    <div className="w-full md:p-4 lg:w-1/2 mx-auto lg:mt-4">
+      <div className="w-full md:rounded-[20px] lg:rounded-[40px] border sm:shadow-lg bg-white text-gray-900 p-[2px] ">
+        <div className="py-3 text-center bg-[#18bfe7] text-white font-black w-full  md:rounded-t-[20px] lg:rounded-t-[40px] lg:text-[40px] sm:text-[28px]">
+          September 2022
+        </div>
+        <div className="lg:text-[24px] sm:text-[16px] my-8 text-[#444444]">
+          <div className="flex items-center w-full justify-center font-medium">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Sun
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Mon
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Tue
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Wed
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Thu
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Fri
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              Sat
+            </div>
+          </div>
+
+          <div className="flex items-center w-full justify-center font-light">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              28
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              29
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              30
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              31
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              1
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              2
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              3
+            </div>
+          </div>
+          <div className="flex items-center w-full  font-light justify-center">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              4
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              5
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              6
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              7
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              8
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              9
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              10
+            </div>
+          </div>
+          <div className="flex items-center w-full  font-light justify-center">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              11
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              12
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              13
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              14
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              15
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              16
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              17
+            </div>
+          </div>
+          <div className="flex items-center w-full  font-light justify-center">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              18
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              19
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              20
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              21
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              22
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              23
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              24
+            </div>
+          </div>
+          <div className="flex items-center w-full font-light justify-center">
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              25
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              26
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              27
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              28
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              29
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              30
+            </div>
+            <div className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+              1
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
